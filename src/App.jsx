04be841015f394b6349212a80b65bc59ae416b31
Web-Chain-Dev <frolov.milan@gmail.com>
2024-06-21 15:48:04 +0300

    lighting change

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <div className="absolute pink__gradient top-[1400px] right-40 w-[40%] h-[40%] rounded-full " />
       <div className="absolute white__gradient top-[2800px] w-[50%] h-[50%] rounded-full opacity-50" />
       <div className="absolute blue__gradient top-[2300px] right-40 w-[40%] h-[40%] rounded-full opacity-50" />
-      <div className="absolute navy__gradient top-[2500px] w-[40%] h-[40%] rounded-full opacity-50" />
+      <div className="absolute navy__gradient top-[2450px] left-[25%] w-[40%] h-[40%] rounded-full opacity-50" />
       <div className="absolute green__gradient top-[2800px] right-40 w-[40%] h-[40%] rounded-full opacity-30" />
 
       <div className="flex flex-col z-[20]">
